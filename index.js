85c55949586a2b2694b4809ff5573d21aa3da447
Chris Voss <chrisvoss92@gmail.com>
2017-07-06 13:40:54 -0700

    User can update posts.

--- index.js
+++ index.js
@@ -20,6 +20,19 @@ app.get('/notes', (req, res) => {
   res.send(notes)
 })
 
+app.put('/notes/:id', (req, res) => {
+  console.log(req.params)
+  const itemId = parseInt(req.params.id, 10)
+  const item = notes.find(item => {
+    return item.id === itemId
+  })
+  if (!item) {
+    return res.sendStatus(404)
+  }
+  Object.assign(item, req.body)
+  res.sendStatus(200)
+})
+
 app.listen(3000, () => {
   console.log('Listening on 3000!')
 })
